6c07e465d35d6113ee26adee670fd1bf1b5c1692
Simon Le Cleach <slecleach@theaiinstitute.com>
2023-08-26 16:28:26 -0400

    adding new icycle

--- mjpc/tasks/tasks.cc
+++ mjpc/tasks/tasks.cc
@@ -50,7 +50,6 @@ std::vector<std::shared_ptr<Task>> GetTasks() {
     std::make_shared<Quadrotor>(),
     std::make_shared<QuadrupedFlat>(),
     std::make_shared<QuadrupedHill>(),
-    std::make_shared<Bike>(),
     std::make_shared<Swimmer>(),
     std::make_shared<Bike>(),
     std::make_shared<Walker>(),
